bc7ca60d101335ebc58fb3b81588288658f3db15
Sam Stoelinga <sammiestoel@gmail.com>
2023-06-10 18:22:04 +0000

    Reduce falcon-7b-instruct image size

--- falcon-7b-instruct/build.py
+++ falcon-7b-instruct/build.py
@@ -1,13 +1,8 @@
 # Download huggingface transformers and load and save the model and tokenizer to disk for later inference and finetuning.
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from huggingface_hub import snapshot_download
 
 model_id = "tiiuae/falcon-7b-instruct"
 
-model = AutoModelForCausalLM.from_pretrained(
-    model_id, load_in_4bit=True, device_map="auto", trust_remote_code=True)
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-
-save_path = '/built/'
-model.save_pretrained(save_path)
-tokenizer.save_pretrained(save_path)
+snapshot_download(repo_id=model_id, local_dir="/built", local_dir_use_symlinks=False,
+        ignore_patterns="*weight.bin")
