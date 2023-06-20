6654d6ed5786f8b13185787b6e6f798f9343ed83
Sam Stoelinga <sammiestoel@gmail.com>
2023-06-20 06:37:57 +0000

    move falcon-7b-instruct to separate repo

--- falcon-7b-instruct/build.py
+++
@@ -1,8 +0,0 @@
-# Download huggingface transformers and load and save the model and tokenizer to disk for later inference and finetuning.
-
-from huggingface_hub import snapshot_download
-
-model_id = "tiiuae/falcon-7b-instruct"
-
-snapshot_download(repo_id=model_id, local_dir="/built", local_dir_use_symlinks=False,
-        ignore_patterns="*weight.bin")
--- falcon-7b-instruct/serve.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-set -xe
-
-PYTHONUNBUFFERED=1 python -m basaran
